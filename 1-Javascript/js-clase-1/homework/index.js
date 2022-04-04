7af5e5cf8555b852271b6143b5ea405f9050d32b
Nahuel Rojas <nadaro@outlook.com>
2022-04-04 16:04:27 -0300

    javascript/clase1

---
+++ 1-Javascript/js-clase-1/homework/index.js
@@ -0,0 +1,15 @@
+/*******************************************************************************
+	Realizar una función llamada “calculatePrice” que reciba dos parámetros,
+	el primero siendo el nombre de un producto (ej: Laptop), y el segundo 
+	el precio de ese producto (sin el signo $). Dicha función deberá retornar 
+	un string con el nombre del producto, el costo del envío del mismo, y el precio final.
+*******************************************************************************/
+//Tu código acá
+
+
+
+
+
+module.exports = {
+	calculatePrice,
+};
