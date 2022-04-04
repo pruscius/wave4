b6deb65283c037e421560188b3e2b6b3e9ace0c9
Nahuel Rojas <nadaro@outlook.com>
2022-04-04 18:39:43 -0300

    js-clase1

--- 1-Javascript/js-clase-1/homework/index.js
+++ 1-Javascript/js-clase-1/homework/index.js
@@ -1,8 +1,13 @@
 /*******************************************************************************
-	Realizar una función llamada “calculatePrice” que reciba dos parámetros,
-	el primero siendo el nombre de un producto (ej: Laptop), y el segundo 
-	el precio de ese producto (sin el signo $). Dicha función deberá retornar 
-	un string con el nombre del producto, el costo del envío del mismo, y el precio final.
+Realizar una función llamada “calculatePrice” que reciba dos parámetros,
+el primero siendo el nombre de un producto (ej: "Laptop""), y el segundo 
+el precio de ese producto (sin el signo $). Dicha función deberá retornar 
+un string con el nombre del producto, el costo del envío del mismo, y el precio final.
+
+Ejemplo:
+calculatePrice("play", 30000)
+
+output : "el costo de envio de una play es de 500 pesos y el precio total seria de 30500 pesos"
 *******************************************************************************/
 //Tu código acá
 
